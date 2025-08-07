dd8d7b0da0aa20492e98da0dca5da86ebc481797
Abhishek <abhishekgautam1966@gmail.com>
2025-08-07 11:19:58 +0530

    made changes for vercel deployment

--- index.js
+++ index.js
@@ -1,36 +1,64 @@
 const {makeDbConnection} = require("./events.dbConnection")
 const Event = require("./events.model")
 
-const fs = require("fs")
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+const cors = require("cors")
+const corsOptions = {
+    origin: "*", 
+    credentials: true,
+}
+app.use(cors(corsOptions))
+
 makeDbConnection()
 
-const jsonData = fs.readFileSync("./events.json", "utf-8")
-const eventsData = JSON.parse(jsonData)
 
-function seedEventData(){
+const readAllEvents = async () => {
     try{
-    for(const event of eventsData){
-        const newEvent = new Event({
-            name: event.name,
-            type: event.type,
-            date: event.date,
-            startingTime: event.startingTime,
-            endingTime: event.endingTime,
-            hostName: event.hostName,
-            eventDetails: event.eventDetails,
-            eventImgUrl: event.eventImgUrl,
-            additionalInfo: event.additionalInfo,
-            tags: event.tags,
-            address: event.address,
-            price: event.price,
-            speakers: event.speakers,            
-        })
-        newEvent.save()
-    }
+        const allEvents = await Event.find()
+        return allEvents
     }catch(error){
-        console.log("error occured while seeding data", error)
+        console.log("error occured while fetching events", error)
     }
 }
 
+app.get("/events", async (req, res) => {
+    try{
+        const allEventsDetails = await readAllEvents();
+        if(allEventsDetails){
+            res.status(200).json(allEventsDetails)
+        }else{
+            res.status(404).json({error: "events not found."})
+        }
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
 
-seedEventData()
+
+const readEventById = async (eventId) => {
+    try{
+        const selectedEvent = await Event.findById(eventId)
+        return selectedEvent
+    }catch(error){
+        console.log(error)
+    }
+}
+
+app.get("/events/:id", async (req, res) => {
+    try{
+        const myEvent = await readEventById(req.params.id)
+        res.status(200).json(myEvent)
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log("server is running on port", PORT)
+})
